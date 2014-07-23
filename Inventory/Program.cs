d0ff361fbf85366c8d8243ce1f06cb70b12d6451
bluedog8050 <bluedog8050@hotmail.com>
2014-07-23 08:33:48 -0700

    TeST

--- Inventory/Program.cs
+++ Inventory/Program.cs
@@ -71,7 +71,7 @@ namespace Inventory
         }
 
         /// <summary> 
-        /// Sets the current name of the player. ////This is a superficial change.
+        /// Sets the current name of the player.
         /// </summary>
         bool SetName(params string[] input)
         {
